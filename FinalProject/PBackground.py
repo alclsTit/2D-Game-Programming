ece530d641a0c8ac506a179dc6711a32ddc5ec3e
alclsTit <c936891@gmail.com>
2016-11-08 23:51:37 +0900

    배경 업데이트
    1.픽셀단위 -> 시간단위 움직임으로 변화(2차 수정)

--- FinalProject/PBackground.py
+++ FinalProject/PBackground.py
@@ -1,12 +1,12 @@
 from pico2d import *
 import PCharacter
-
-open_canvas(1500, 900)
+import RES
 
 character = PCharacter.Player()
 
+#open_canvas(1500,900,sync=True)
+
 class Background:
-    Background_image = None
 
     def __init__(self):
         self.x , self.y = 750 , 450  #시작 지점은 해당 사진의 1/2위치
@@ -14,18 +14,14 @@ class Background:
         self.frame_move_second = 0
         self.switching_cnt = 0
 
-        if Background.Background_image == None:
-            Background.Background_image = load_image('FirstStage_s.png')
-
     def Update(self):
-        if(character.move_x >= 500):
-           self.frame_move += character.move_size
+        if(PCharacter.Player.move_x >= 500):
+            self.frame_move += character.move_size
 
         if(self.frame_move % 2500 == 0):
             self.frame_move = 0
 
-
     def Draw(self):
-           self.Background_image.clip_draw(self.frame_move , 0, 1500, 900, self.x , self.y)
+        RES.res.Background_image.clip_draw(self.frame_move , 0, 1500, 900, self.x , self.y)
 
 
