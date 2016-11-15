5076d6e794335d049e77d3b4dda0d44cffcd7716
alclsTit <c936891@gmail.com>
2016-11-15 23:21:25 +0900

    배경 업데이트
    배경에 음악 추가

--- FinalProject/PBackground.py
+++ FinalProject/PBackground.py
@@ -13,6 +13,9 @@ class Background:
         self.frame_move = 0
         self.frame_move_second = 0
         self.switching_cnt = 0
+        self.bgm = load_music('Evans[jubeat]_stage01.mp3')
+        self.bgm.set_volume(64)
+        self.bgm.repeat_play()
 
     def Update(self):
         if(PCharacter.Player.move_x >= 500):
@@ -20,6 +23,12 @@ class Background:
 
         if(self.frame_move % 2500 == 0):
             self.frame_move = 0
+
+        if(PCharacter.Player.Player_Die == True):
+            self.bgm.stop()
+
+    #def background_soundplay(self):
+
 
     def Draw(self):
         RES.res.Background_image.clip_draw(self.frame_move , 0, 1500, 900, self.x , self.y)
