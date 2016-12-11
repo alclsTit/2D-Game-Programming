a79b8dfcfbcb0c5651b2cc5f1da24c57a70d3186
alclsTit <c936891@gmail.com>
2016-12-11 23:48:38 +0900

    [추가 구현]포인트 이펙트 추가구현
    포인트 이펙트 추가구현

--- FinalProject/Effect.py
+++ FinalProject/Effect.py
@@ -1,9 +1,20 @@
+import PCharacter
+import RES
+
+
 class Point_Effect:
-    def __int__(self):
+    def __init__(self):
+        self.x , self.y = 0 , 0
+        self.half_width , self.half_height = 25 , 25
+
         pass
 
     def Update(self):
+        self.x = PCharacter.Player.move_x
+        self.y = PCharacter.Player.move_y + self.half_height + 30
+
         pass
 
     def Draw(self):
+
         pass
