11c98d6077331e294d7f8ee3a1e2e30c4f81ae14
alclsTit <c936891@gmail.com>
2016-12-16 23:50:08 +0900

    [프로젝트 완료상태] 타일코드리팩토링에따른 충돌체크함수수정
    타일 충돌체크 함수 수정

--- FinalProject/Collision.py
+++ FinalProject/Collision.py
@@ -22,3 +22,13 @@ def collide_for_coin(a,b,num):
     if bottom_a > top_b : return False
 
     return True
+
+def collide_for_brick(a,b,num):
+    left_a, bottom_a, right_a, top_a = a.get_bb()
+    left_b, bottom_b, right_b, top_b = b.collide_brick(num)
+    if left_a > right_b : return False
+    if right_a < left_b : return False
+    if top_a < bottom_b : return False
+    if bottom_a > top_b : return False
+
+    return True
